ddb3a13f1673d9c6b0658c01b4b5d0ddfe1e034c
Krisz <kriszrokakaszas@gmail.com>
2019-06-03 18:48:52 +0200

    Fifth Assignment Donw

---
+++ FirstSiWeek/SortString/SortString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            Console.WriteLine(str + "\n");
+            string[] words = str.Split(' ');
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine("\n");
+            Array.Sort(words);
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine("\n");
+            string s = string.Join(" ", words);
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
